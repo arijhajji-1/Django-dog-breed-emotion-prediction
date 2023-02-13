6c760f69dc6237a954cfb555e0c870f5c4d49f70
Asma <asmaboubaker11@gmail.com>
2023-02-13 18:47:15 +0100

    result prints in console

--- titanic/base/views.py
+++ titanic/base/views.py
@@ -1,36 +1,49 @@
 from django.shortcuts import render
 import pickle
+import numpy as np
+from os.path import join
+from keras.applications.resnet  import preprocess_input
+#from keras.preprocessing.image import  img_to_array
+from keras.utils import load_img, img_to_array
+from keras.applications import ResNet50
+from learntools.deep_learning.decode_predictions import decode_predictions
+from IPython.display import Image, display
 
+
+size = 244
+
+def read_and_prep_images(img_paths):
+    imgs = [load_img(img_path, target_size=(224, 224)) for img_path in img_paths]
+    img_array = np.array([img_to_array(img) for img in imgs])
+    output = preprocess_input(img_array)
+    return(output)
+
+
+
+    
 def home(request):
     return render(request, 'index.html')
 
-def getPredictions(pclass, sex, age, sibsp, parch, fare, C, Q, S):
-    model = pickle.load(open('ml_model.sav', 'rb'))
-    scaled = pickle.load(open('scaler.sav', 'rb'))
-
-    prediction = model.predict(scaled.transform([
-        [pclass, sex, age, sibsp, parch, fare, C, Q, S]
-    ]))
+def getPredictions():
+    image_dir = 'C:\\Users\\Asma Ben Boubaker\\Desktop\\TitanicPredictionDjangoML\\titanic\\titanic\\train\\'
     
-    if prediction == 0:
-        return 'no'
-    elif prediction == 1:
-        return 'yes'
-    else:
-        return 'error'
+    img_paths = [join(image_dir, filename) for filename in 
+                            ['1.jpg',
+                            ]]
+    image_size = 224
+    #read_and_prep_images(img_paths, 244, 244)
+    my_model = ResNet50(weights='imagenet')
+    test_data = read_and_prep_images(img_paths)
+    preds = my_model.predict(test_data)
+    
+   
+    most_likely_labels = decode_predictions(preds, top=3, class_list_path='C:\\Users\\Asma Ben Boubaker\\input\\ResNet-50\\imagenet_class_index.json')
+    for i, img_path in enumerate(img_paths):
+        display(Image(img_path))
+        print(most_likely_labels[i])
+    return most_likely_labels
 
 def result(request):
-    pclass = int(request.GET['pclass'])
-    sex = int(request.GET['sex'])
-    age = int(request.GET['age'])
-    sibsp = int(request.GET['sibsp'])
-    parch = int(request.GET['parch'])
-    fare = int(request.GET['fare'])
-    embC = int(request.GET['embC'])
-    embQ = int(request.GET['embQ'])
-    embS = int(request.GET['embS'])
-
-    result = getPredictions(pclass, sex, age, sibsp,
-                            parch, fare, embC, embQ, embS)
+    result = getPredictions()
 
     return render(request, 'result.html', {'result': result})
